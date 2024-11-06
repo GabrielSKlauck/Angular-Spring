3a38a34c8d4ecfc85ac44a427c2975e220af2b80
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-11-06 17:42:50 -0300

    Criação da tabela cliente via Spring

--- api/src/main/java/br/com/projeto/api/modelo/Cliente.java
+++ api/src/main/java/br/com/projeto/api/modelo/Cliente.java
@@ -1,6 +1,8 @@
 package br.com.projeto.api.modelo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -13,7 +15,9 @@ import lombok.Setter;
 public class Cliente {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long codigo;
+
 
     private String nome;
 
